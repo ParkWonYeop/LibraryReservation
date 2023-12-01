d7fa6b3ed3627a3f4c48338890c222d0d73664b7
ParkWonYeop <pwy9679@gnu.ac.kr>
2023-12-01 14:43:14 +0900

    Initial commit

---
+++ src/main/java/com/example/libraryreservation/LibraryReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryReservationApplication.class, args);
+    }
+
+}
